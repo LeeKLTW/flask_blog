3a3363ebf247449c656ee30a5075ef8dc48f902d
LeeKLTW <LeeKunLin@LeeKunLins-MBP.local>
2019-01-21 17:00:48 +0800

    add posts in flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+posts = [
+    {'author': 'Sutskever et al',
+     'title': 'Sequence to sequence learning with neural networks',
+     'content': 'https://arxiv.org/pdf/1406.1078.pdf',
+     'datepost': '2014'
+     },
+
+    {'author': 'Kyunghyun Cho, Bart van Merrienboer, Caglar Gulcehre, Dzmitry Bahdanau, Fethi Bougares, Holger Schwenk, Yoshua Bengio',
+     'title': 'Learning Phrase Representations using RNN Encoder-Decoder for Statistical Machine Translation',
+     'content': 'https://arxiv.org/pdf/1704.04368.pdf',
+     'datepost': '3 Sep 2014'
+     }
+]
+
 @app.route("/")
-
-
 @app.route("/home")
+
 def home():
-    return """
-    """
+    return render_template('home.html',posts=posts)
 
 
 @app.route("/about")
