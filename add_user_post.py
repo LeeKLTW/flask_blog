6d567725e062283c78003a69dfd24dd130b4840b
LeeKLTW <LeeKLTW@gmail.com>
2019-02-26 17:31:26 +0800

    add user

--- add_user_post.py
+++ add_user_post.py
@@ -3,12 +3,23 @@ from flask_blog import db
 from flask_blog import User, Post
 
 user_list = [{'username':'Kevin', 'email':'Kevin@gmail.com','password':'password'}]
-u = User(**user_list[0])
+user = User(**user_list[0])
 db.session.add(u)
 db.session.commit()
+
 
 # from flask_sqlalchemy import SQLAlchemy
 # db = SQLAlchemy()
 # User.query.all()
 # User.query.filter_by(username='Kevin').all()
+# user = User.query.first()
+# user.id
+# user = User.query.get(1)
+# user.posts
 
+from flask_blog import Post
+post_1 = Post(title='The Art of Data Science', content='Data science is not only science',user_id=user.id)
+from flask_blog import db
+db.session.add(post_1)
+db.session.commit()
+Post.query.all()
