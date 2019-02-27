14a5db65e940e2a60e33e8e921786b8bbb8d0290
LeeKLTW <LeeKLTW@gmail.com>
2019-02-27 14:55:52 +0800

    init validate on existing user

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,14 +1,19 @@
 # encoding: utf-8
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_blog.models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign up')
+    username = StringField(label='Username', validators=[DataRequired(), Length(min=2, max=10)])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    confirm_password = PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(label='Sign up')
+
+    def validate_username(self,username):
+        if True:
+            raise ValidationError(message='Validation Message.')
 
 
 class LoginForm(FlaskForm):
