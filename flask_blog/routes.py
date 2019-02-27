b5c3996ecb27b0d684f6f4288ad852b44722ec87
LeeKLTW <LeeKLTW@gmail.com>
2019-02-27 16:34:35 +0800

    + "return" redirected(url_for('home')

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -35,7 +35,7 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        redirect(url_for('home'))
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
