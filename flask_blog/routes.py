f8d5d852c092fbacd911d64076292e6b6881686a
LeeKLTW <LeeKLTW@gmail.com>
2019-02-28 18:10:25 +0800

    little syntax fix

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -87,6 +87,7 @@ def account():
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
+            print(picture_file)
             current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
