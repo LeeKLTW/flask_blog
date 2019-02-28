cd55d2434cc6f12498da6073e9d5842fbea75afa
LeeKLTW <LeeKLTW@gmail.com>
2019-02-28 18:27:49 +0800

    "image_file"

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -77,8 +77,9 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, file_extension = os.path.splitext(form_picture.filename)
     picture_filename = random_hex + file_extension
-    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_filename)
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
     return picture_path
+
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
@@ -91,11 +92,13 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated',category='success')
-        return redirect(url_for('account')) # This is needed to avoid POST again
+        flash('Your account has been updated', category='success')
+        return redirect(url_for('account'))  # This is needed to avoid POST again
     # Show current data in form
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    img_file = url_for('static', filename=f'./profile_pics/{current_user.image_file}')
-    return render_template('account.html', title='Account', img_file=img_file, form=form)
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    # image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+
+    return render_template('account.html', title='Account', img_file=image_file, form=form)
