c178941beb5d817111f87d0a5b15bd87684a0ea2
LeeKLTW <LeeKLTW@gmail.com>
2019-04-12 12:16:43 +0800

    logger & html example

--- first_class_function.py
+++ first_class_function.py
@@ -13,19 +13,3 @@ def my_map(func, arg_list):
 square_list = my_map(square, [0,1,2])
 qube_list = my_map(qube,[0,1,2])
 
-def logger(msg):
-    def log_message():
-        print('Log', msg)
-    return log_message
-
-log_hi = logger('Hi') # <function logger.<locals>.log_message at 0x10a096048>
-log_hi() # 'Log Hi'
-
-def html_tag(tag):
-
-    def wrap_text(msg):
-        print('<{0}>{1}</{0}>'.format(tag,msg))
-    return wrap_text
-
-html = html_tag('h1')
-html('Title')
