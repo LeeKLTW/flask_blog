ee63bd77b536fd5d8850f5faefbf67cd9f2c455b
LeeKLTW <LeeKLTW@gmail.com>
2019-04-12 14:21:23 +0800

    closure & log

--- closure.py
+++ closure.py
@@ -1,11 +1,35 @@
 # encoding: utf-8
+# A closure is a function that has access to outer function local variables, even outer function is done execution. Close free variables from enviroment.
+# Closures can avoid the use of global values and provides some form of data hiding. It can also provide an object oriented solution to the problem.
 
-def outer_func():
-    message = 'Hi'
-    def inner_func():
-        print(message)
+import logging
 
-    return inner_func
+logging.basicConfig(filename='closure.log', level=logging.DEBUG, format="[%(levelname)-8s] - %(message)s")
 
-outer_func()
-outer_func()()
+def logger(func):
+    def log_func(*args):
+        result = func(*args)
+        logging.info('Function: "{}"  with arguments "{}" get result "{}"'.format(func.__name__, args,result))
+    return log_func
+
+
+# def add(x, y):
+#     return x + y
+
+def add(*args):
+    return sum(args)
+
+
+def sub(x, y):
+    return x - y
+
+
+add_logger = logger(add)
+sub_logger = logger(sub)
+add_logger(1,2,3,4,5)
+add_logger(69,73)
+add_logger(69,73,88)
+
+sub_logger(10, 2)
+sub_logger(1, 1e10)
+sub_logger(1, 1e-10)
